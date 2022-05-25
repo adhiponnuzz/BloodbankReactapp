cfeccb41838735e360aab0734dd5f63abdc97556
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-25 16:37:35 +0530

    header finished

--- src/component/Signin.js
+++ src/component/Signin.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Header from './Header'
 
 const Signin = () => {
     var [username,setUsername]=useState("")
@@ -12,6 +13,7 @@ const Signin = () => {
 
   return (
     <div>
+        <Header/>
 
 <div className="container">
     <div className="row">
